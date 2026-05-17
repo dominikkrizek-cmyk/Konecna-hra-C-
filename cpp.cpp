fc46e15ac819da55a4ab3dbaeb506274bf4c9e6e
dominikkrizek-cmyk <dominik.krizek@skolakrizik.cz>
2026-05-17 21:51:48 +0200

    Add files via upload

--- cpp.cpp
+++ cpp.cpp
@@ -72,3 +72,56 @@ void showPlayer(const Player& p) {
     cout << "Utok: " << p.attack << endl;
     cout << "Zlato: " << p.gold << endl;
 }
+Player createClass(string name, int hp, int mana, int attack, vector<Ability> abilities) {
+    Player p;
+    p.className = name;
+    p.maxHp = hp;
+    p.hp = hp;
+    p.maxMana = mana;
+    p.mana = mana;
+    p.attack = attack;
+    p.gold = 20;
+    p.level = 1;
+    p.xp = 0;
+    p.abilities = abilities;
+    p.paralyzed = false;
+    return p;
+}
+
+Player chooseClass() {
+    while (true) {
+        cout << "\nVyber si classu:\n";
+        cout << "1) Paladin\n";
+        cout << "2) Lovec\n";
+        cout << "3) Mag\n";
+        cout << "4) Warlock\n";
+        cout << "Volba: ";
+
+        int choice = getChoice(1, 4);
+        Player p;
+
+        if (choice == 1) {
+            p = createClass("Paladin", 30, 12, 5, {
+                {"Uder", 5, 0, false, false},
+                {"Uder svetlem", 3, 3, true, false},
+                {"Oziveni", 6, 2, false, true}
+            });
+        } else if (choice == 2) {
+            p = createClass("Lovec", 24, 15, 6, {
+                {"Vystrel", 6, 0, false, false},
+                {"Sipovy dest", 4, 4, true, false},
+                {"Prvni pomoc", 5, 3, false, true}
+            });
+        } else if (choice == 3) {
+            p = createClass("Mag", 20, 22, 4, {
+                {"Magicka strela", 5, 0, false, false},
+                {"Ohniva vlna", 6, 5, true, false},
+                {"Leciva magie", 7, 4, false, true}
+            });
+        } else {
+            p = createClass("Warlock", 26, 18, 5, {
+                {"Temny uder", 5, 0, false, false},
+                {"Prokleti", 5, 4, true, false},
+                {"Krvavy ritual", 8, 5, false, true}
+            });
+        }
